531a899e05575e5ba8c15e7fa1afcb50753a24f6
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2023-07-19 23:18:03 +0800

    chore: add firebase config file

--- public/assets/js/firebase/firebase.js
+++ public/assets/js/firebase/firebase.js
@@ -1,18 +1,14 @@
 // Initialize Firebase
 
 var config = {
-  apiKey: "AIzaSyD-J2HLqV3WVjqJZ6T1wJCpvBmezZI87iA",
-  authDomain: "hank199599.firebaseapp.com",
-  databaseURL: "https://hank199599.firebaseio.com",
-  projectId: "hank199599",
-  storageBucket: "hank199599.appspot.com",
-  messagingSenderId: "382026713515",
-  appId: "1:382026713515:web:5cd379be41c149bc31eaee",
-  measurementId: "G-XRG9Z7KJ2Q"
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url-id>',
+  projectId: '<your-project-id>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>',
+  appId:  '<your-app-id>',
+  measurementId: '<your-measurement-id>',
 };
 
 firebase.initializeApp(config);
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
